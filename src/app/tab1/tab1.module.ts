7fc39dfd1798f25957e7ed14181e4b238ed9a089
Farogh <farogh007@gmail.com>
2019-06-02 10:29:28 +0530

    graphs added locally

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+    Ng2GoogleChartsModule
   ],
   declarations: [Tab1Page]
 })
